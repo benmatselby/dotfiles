e3b0a7a8740387b1c88ba6cd1f688f3ba4445ec0
Ben Selby <benmatselby@gmail.com>
2022-04-26 18:06:57 +0100

    Install yarn globally.

--- node/install.sh
+++ node/install.sh
@@ -6,5 +6,6 @@ NODE_PACKAGES=(
   http-server
   netlify-cli
   @redocly/openapi-cli
+  yarn
 )
 for pkg in "${NODE_PACKAGES[@]}"; do printf "installing %s\n" "${pkg}" && npm install -g "${pkg}"; done
