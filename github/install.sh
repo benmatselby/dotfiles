f531312c9054dc5661a148dfa6b717e4825670e1
Ben Selby <benmatselby@gmail.com>
2022-07-04 21:00:26 +0100

    Add configuration for `gh dash`

--- github/install.sh
+++ github/install.sh
@@ -7,3 +7,6 @@ gh extension install dlvhdr/gh-dash || true
 
 # Upgrades
 gh extension upgrade --all
+
+# Config
+ln -sf "${DOTFILES_LOCATION}/github/gh-dash.yml" "${HOME}/.config/gh-dash/config.yml"
