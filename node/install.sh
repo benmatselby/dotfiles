1f0cfd9776901cee1abce936a1872d583727b9e9
Ben Selby <benmatselby@gmail.com>
2022-11-09 09:07:50 +0000

    Install `ttab`

--- node/install.sh
+++ node/install.sh
@@ -6,6 +6,7 @@ NODE_PACKAGES=(
   http-server
   netlify-cli
   @redocly/openapi-cli
+  ttab
   yarn
 )
 for pkg in "${NODE_PACKAGES[@]}"; do printf "installing %s\n" "${pkg}" && npm install -g "${pkg}"; done
