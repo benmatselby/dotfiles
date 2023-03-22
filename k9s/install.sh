1e1a087fc3e959081b76cd1c20b743dec937174e
Ben Selby <benmatselby@gmail.com>
2023-03-22 14:39:45 +0000

    k9s config - Use the same stuff as iTerm profiles.

--- k9s/install.sh
+++ k9s/install.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-ln -sf "${DOTFILES_LOCATION}/k9s/monokai.toml" "${HOME}/Library/Application Support/k9s/skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/monokai.yml" "${HOME}/Library/Application Support/k9s/skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/prod_skin.yml" "${HOME}/Library/Application Support/k9s/prod_skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/inc_skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/test_skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/staging_skin.yml"
