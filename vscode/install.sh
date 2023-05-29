85f4ae2ba173ed2e5d2c8eb7786253a0d848265f
Ben Selby <benmatselby@gmail.com>
2023-05-29 16:23:41 +0100

    Add the GitHub Actions extension
    And turn off some other things.

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,16 +6,17 @@ ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application 
 ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
 
 CODE_EXTENSIONS=(
-  bmewburn.vscode-intelephense-client
+  # bmewburn.vscode-intelephense-client
   DavidAnson.vscode-markdownlint
   dbaeumer.vscode-eslint
   esbenp.prettier-vscode
   firsttris.vscode-jest-runner
   # felixfbecker.php-intellisense
   github.remotehub
+  github.vscode-github-actions
   github.vscode-pull-request-github
   golang.go
-  hashicorp.terraform
+  # hashicorp.terraform
   ms-azuretools.vscode-docker
   ms-python.isort
   ms-python.python
@@ -27,6 +28,6 @@ CODE_EXTENSIONS=(
   streetsidesoftware.code-spell-checker
   # timonwong.shellcheck
   Tyriar.sort-lines
-  xdebug.php-debug
+  # xdebug.php-debug
 )
 for ext in "${CODE_EXTENSIONS[@]}"; do printf "installing %s\n" "${ext}" && code --install-extension "${ext}" --force; done
