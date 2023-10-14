c62167aad2550c9e0dd960b64fa5c1fc6c08681d
Ben Selby <benmatselby@gmail.com>
2023-10-14 20:20:01 +0100

    Support for Sublime Text

---
+++ sublime-text/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+set -e
+
+ln -sf "${DOTFILES_LOCATION}/sublime-text/Default (OSX).sublime-keymap" "${HOME}/Library/Application Support/Sublime Text/Packages/User/Default (OSX).sublime-keymap"
