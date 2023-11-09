b3396a34a0d8c0f2e5652ebb06072b0cabe5733c
Ben Selby <benmatselby@gmail.com>
2023-11-09 09:22:47 +0000

    Symlink in for more k8s contexts

--- k9s/install.sh
+++ k9s/install.sh
@@ -5,5 +5,6 @@ set -e
 ln -sf "${DOTFILES_LOCATION}/k9s/monokai.yml" "${HOME}/Library/Application Support/k9s/skin.yml"
 ln -sf "${DOTFILES_LOCATION}/k9s/prod_skin.yml" "${HOME}/Library/Application Support/k9s/prod_skin.yml"
 ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/inc_skin.yml"
+ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/rp_skin.yml"
 ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/test_skin.yml"
 ln -sf "${DOTFILES_LOCATION}/k9s/testing_skin.yml" "${HOME}/Library/Application Support/k9s/staging_skin.yml"
