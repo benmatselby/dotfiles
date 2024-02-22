e7d5ad446896aa4419bf18c27f1549b24f831274
Ben Selby <ben.selby@prolific.com>
2024-02-22 19:09:28 +0000

    Make sure the folders are present

--- github/install.sh
+++ github/install.sh
@@ -10,6 +10,7 @@ gh extension install github/gh-copilot || true
 gh extension upgrade --all
 
 # Config
+mkdir -p "${HOME}/.config/gh-dash/"
 ln -sf "${DOTFILES_LOCATION}/github/gh-dash.yml" "${HOME}/.config/gh-dash/config.yml"
 mkdir -p "${HOME}/.config/gh/"
 ln -sf "${DOTFILES_LOCATION}/github/gh-config.yml" "${HOME}/.config/gh/config.yml"
