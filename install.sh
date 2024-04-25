4871df70c40366f576ad62a103d9d2e9055ed5c6
Ben Selby <ben.selby@prolific.com>
2024-04-25 07:39:51 +0100

    Add terraform to prompt

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ export DOTFILES_LOCATION;
 ./bin/dotfiles install github
 ./bin/dotfiles install node
 ./bin/dotfiles install mongodb
-./bin/dotfiles install php
+#./bin/dotfiles install php
 ./bin/dotfiles install k9s
 ./bin/dotfiles install starship
 ./bin/dotfiles install tmux
