ec072d6e89d10c9a3f6b972b23c7be1879edd3b4
Ben Selby <benmatselby@gmail.com>
2024-05-24 21:19:58 +0100

    Ask where the ssh key is for signing git commits

--- git/install.sh
+++ git/install.sh
@@ -6,12 +6,16 @@ if [ ! -f "${HOME}/.gitconfig.local" ] ; then
   cp "${DOTFILES_LOCATION}/git/.gitconfig.local" "${HOME}/.gitconfig.local"
 
   echo "Enter your full name";
-  read -re var
-  sed -i '' "s/GITNAME/${var}/" "${HOME}/.gitconfig.local"
+  read -r var
+  sed -i '.orig' "s|GITNAME|${var}|" "${HOME}/.gitconfig.local"
 
   echo "Enter your email address";
-  read -re var
-  sed -i '' "s/GITEMAIL/${var}/" "${HOME}/.gitconfig.local"
+  read -r var
+  sed -i '' "s|GITEMAIL|${var}|" "${HOME}/.gitconfig.local"
+
+  echo "Entire the path to your public ssh key"
+  read -r var
+  sed -i '' "s|GITSIGNKEY|${var}|" "${HOME}/.gitconfig.local"
 fi
 
 ln -sf "${DOTFILES_LOCATION}/git/.gitconfig" "${HOME}/.gitconfig"
