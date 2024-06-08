661ab843ce0f0fb815da7f125eb926058f3f6fae
Ben Selby <benmatselby@gmail.com>
2024-06-08 22:26:01 +0100

    Move tmux themes to their own folder/configs

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+mkdir -p "${HOME}/.config/tmux/"
+
 ln -sf "${DOTFILES_LOCATION}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${DOTFILES_LOCATION}/tmux/themes" "${HOME}/.config/tmux/themes"
