8a1adbc7e5d3e9de3bac41264e20802231c8602a
Ben Selby <benmatselby@gmail.com>
2024-06-09 13:22:58 +0100

    Use full / path for creating tmux theme symlink

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,4 +5,4 @@ set -e
 mkdir -p "${HOME}/.config/tmux/"
 
 ln -sf "${DOTFILES_LOCATION}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
-ln -sf "${DOTFILES_LOCATION}/tmux/themes" "${HOME}/.config/tmux/themes"
+ln -sf "${DOTFILES_LOCATION}/tmux/themes/" "${HOME}/.config/tmux/themes/"
