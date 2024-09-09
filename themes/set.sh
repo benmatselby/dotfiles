add3c485bad6a2d3c79b0ec3831d40815388f3b4
Ben Selby <benmatselby@gmail.com>
2024-09-09 21:57:45 +0100

    Provide a theme set script
    Pulled 3 themes from omakub https://github.com/basecamp/omakub

---
+++ themes/set.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Get first argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <theme>"
+  exit 1
+fi
+
+# Set theme
+THEME=$1
+
+echo "Setting background to ${THEME}"
+echo "${PWD}/themes/${THEME}/background.png"
+osascript -e "tell application \"System Events\" to set picture of every desktop to \"${PWD}/themes/${THEME}/background.png\""
