dfabb6fa5ad70f81acd5fbeb1a888735fd3533f6
Ben Selby <benmatselby@gmail.com>
2024-12-19 19:31:15 +0000

    Only set tmux when setting themes now

--- themes/set.sh
+++ themes/set.sh
@@ -11,17 +11,17 @@ THEME=$1
 echo "Setting theme to ${THEME}"
 
 # Backgrounds
-osascript -e "tell application \"System Events\" to set picture of every desktop to \"${PWD}/themes/${THEME}/background.png\""
+#osascript -e "tell application \"System Events\" to set picture of every desktop to \"${PWD}/themes/${THEME}/background.png\""
 
 # Neovim
-mkdir -p ~/.config/nvim/lua/plugins
-cp "${PWD}/themes/${THEME}/neovim.lua" ~/.config/nvim/lua/plugins/theme.lua
+# mkdir -p ~/.config/nvim/lua/plugins
+# cp "${PWD}/themes/${THEME}/neovim.lua" ~/.config/nvim/lua/plugins/theme.lua
 
 # VSCode
-source "${PWD}/themes/${THEME}/vscode.sh"
-sed -i '' "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"${VSC_THEME_DARK}\"/g" "${PWD}/vscode/settings.json"
-sed -i '' "s/\"workbench.preferredDarkColorTheme\": \".*\"/\"workbench.preferredDarkColorTheme\": \"${VSC_THEME_DARK}\"/g" "${PWD}/vscode/settings.json"
-sed -i '' "s/\"workbench.preferredLightColorTheme\": \".*\"/\"workbench.preferredLightColorTheme\": \"${VSC_THEME_LIGHT}\"/g" "${PWD}/vscode/settings.json"
+# source "${PWD}/themes/${THEME}/vscode.sh"
+# sed -i '' "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"${VSC_THEME_DARK}\"/g" "${PWD}/vscode/settings.json"
+# sed -i '' "s/\"workbench.preferredDarkColorTheme\": \".*\"/\"workbench.preferredDarkColorTheme\": \"${VSC_THEME_DARK}\"/g" "${PWD}/vscode/settings.json"
+# sed -i '' "s/\"workbench.preferredLightColorTheme\": \".*\"/\"workbench.preferredLightColorTheme\": \"${VSC_THEME_LIGHT}\"/g" "${PWD}/vscode/settings.json"
 
 # Tmux
 ln -sf "${PWD}/themes/${THEME}/tmux.conf" ~/.config/tmux/theme.conf
