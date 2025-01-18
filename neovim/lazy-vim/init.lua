f63ff1cac59c5bb8cb24ab40ca195d509f6939a3
Ben Selby <benmatselby@gmail.com>
2025-01-18 17:39:21 +0000

    Trim whitespace on save for neovim

--- neovim/lazy-vim/init.lua
+++ neovim/lazy-vim/init.lua
@@ -4,3 +4,11 @@ require("config.lazy")
 -- Give me the hash symbol
 vim.api.nvim_set_keymap("i", "<M-3>", "#", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<M-3>", "#", { noremap = true, silent = true })
+
+-- Trim trailing whitespace on save
+vim.cmd([[
+  augroup TrimWhitespace
+    autocmd!
+    autocmd BufWritePre * %s/\s\+$//e
+  augroup END
+]])
