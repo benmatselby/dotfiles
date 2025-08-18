7763b6ed783e692aae16d27a20e64fa7d82757b8
Ben Selby <benmatselby@gmail.com>
2025-08-18 22:22:35 +0100

    Update super-tab config for neovim

--- neovim/lazy-vim/lua/plugins/completion.lua
+++ neovim/lazy-vim/lua/plugins/completion.lua
@@ -1,6 +1,15 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    keymap = { preset = "super-tab" },
+    keymap = {
+      -- This is only here because of this bug
+      -- https://github.com/LazyVim/LazyVim/issues/6185
+      preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+    },
   },
 }
