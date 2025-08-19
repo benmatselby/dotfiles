2b3b452c6e96c28439cce9e7aa5fb003ffbd6dda
Ben Selby <benmatselby@gmail.com>
2025-08-19 23:22:02 +0100

    Update options for neovim

--- neovim/lazy-vim/lua/config/options.lua
+++ neovim/lazy-vim/lua/config/options.lua
@@ -2,8 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.relativenumber = true
-
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "100,120"
 vim.go.background = "dark"
