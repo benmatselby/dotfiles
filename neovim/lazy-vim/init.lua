6674a2fc006bd8410cb678bb4779ab6726986c39
Ben Selby <benmatselby@gmail.com>
2025-08-28 07:05:03 +0100

    Update neovim plugins

--- neovim/lazy-vim/init.lua
+++ neovim/lazy-vim/init.lua
@@ -4,6 +4,7 @@ require("config.lazy")
 -- Give me the hash symbol
 vim.api.nvim_set_keymap("i", "<M-3>", "#", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<M-3>", "#", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("c", "<M-3>", "#", { noremap = true, silent = true })
 
 -- Trim trailing whitespace on save
 vim.cmd([[
