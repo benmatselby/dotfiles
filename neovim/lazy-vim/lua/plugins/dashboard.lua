ec1969a129ee28344eb7998b99dccd98935c25a2
Ben Selby <benmatselby@gmail.com>
2025-09-18 21:01:24 +0100

    Add custom dashboard logo

---
+++ neovim/lazy-vim/lua/plugins/dashboard.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      preset = {
+        header = [[
+██████╗ ███╗   ███╗███████╗
+██╔══██╗████╗ ████║██╔════╝
+██████╔╝██╔████╔██║███████╗
+██╔══██╗██║╚██╔╝██║╚════██║
+██████╔╝██║ ╚═╝ ██║███████║
+╚═════╝ ╚═╝     ╚═╝╚══════╝
+        ]],
+      },
+    },
+  },
+}
