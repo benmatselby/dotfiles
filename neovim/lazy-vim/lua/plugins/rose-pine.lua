187423d4f6ecdb8175c338a593af596124121e7d
Ben Selby <benmatselby@gmail.com>
2025-09-18 21:57:41 +0100

    Use the moon variant for rose-pine in dark mode

--- neovim/lazy-vim/lua/plugins/rose-pine.lua
+++ neovim/lazy-vim/lua/plugins/rose-pine.lua
@@ -2,6 +2,10 @@ return {
   "rose-pine/neovim",
   name = "rose-pine",
   config = function()
+    require("rose-pine").setup({
+      variant = "auto",
+      dark_variant = "moon",
+    })
     vim.cmd("colorscheme rose-pine")
   end,
 }
