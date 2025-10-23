ee4eff29d11bf66b400fa406e53d13176419155d
Ben Selby <benmatselby@gmail.com>
2025-10-23 19:54:39 +0100

    Stop faffing with moving cursor when paging up/down in nvim

--- neovim/lazy-vim/lua/config/keymaps.lua
+++ neovim/lazy-vim/lua/config/keymaps.lua
@@ -6,14 +6,6 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Keep the cursor in the middle of the screen
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- Keep the cusor in the middle when word searching
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
 -- Copy filename
 vim.keymap.set("n", "<leader>fz", function()
   local filename = vim.fn.expand("%")
