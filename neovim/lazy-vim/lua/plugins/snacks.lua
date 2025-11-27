616ced15ddbcf5fc57fedd0c858bc331506487c6
Ben Selby <benmatselby@gmail.com>
2025-11-27 18:32:46 +0000

    Consolidate where the snacks plugin config lives

--- neovim/lazy-vim/lua/plugins/snacks.lua
+++ neovim/lazy-vim/lua/plugins/snacks.lua
@@ -12,9 +12,41 @@ return {
  ██╔══██╗██║╚██╔╝██║╚════██║
  ██████╔╝██║ ╚═╝ ██║███████║
  ╚═════╝ ╚═╝     ╚═╝╚══════╝
-Never give up, never surrender!
+Never give up, never surrender!!
         ]],
       },
+    },
+    gh = { enabled = true },
+    picker = { enabled = true },
+  },
+  keys = {
+    {
+      "<leader>gi",
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = "GitHub Issues (open)",
+    },
+    {
+      "<leader>gI",
+      function()
+        Snacks.picker.gh_issue({ state = "all" })
+      end,
+      desc = "GitHub Issues (all)",
+    },
+    {
+      "<leader>gp",
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = "GitHub Pull Requests (open)",
+    },
+    {
+      "<leader>gP",
+      function()
+        Snacks.picker.gh_pr({ state = "all" })
+      end,
+      desc = "GitHub Pull Requests (all)",
     },
   },
 }
