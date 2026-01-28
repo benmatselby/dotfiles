0abfaa08e5af6cecbd0790eb4fc443de49f8a702
Ben Selby <benmatselby@gmail.com>
2026-01-28 19:54:04 +0000

    Remove crush
    I'm all in on claude at this point

--- crush/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-echo "🚀 crush configuration"
-
-mkdir -p "${HOME}/.config/crush"
-
-ln -sf "${PWD}/crush/crush.json" "${HOME}/.config/crush/crush.json"
--- install.sh
+++ install.sh
@@ -16,7 +16,6 @@ set -e
 ./tmux/install.sh
 ./ghostty/install.sh
 ./sqlite/install.sh
-./crush/install.sh
 ./claude/install.sh
 
 ###
