605ad04e9531fbd294414b2735a9070084997819
Ben Selby <benmatselby@gmail.com>
2026-02-20 20:28:33 +0000

    Run the nvim-make plugin

---
+++ neovim/lazy-vim/lua/plugins/make.lua
@@ -0,0 +1,16 @@
+return {
+  "benmatselby/nvim-make",
+  dir = "~/git/github/benmatselby/nvim-make/",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "NvimMake",
+  opts = {},
+  keys = {
+    {
+      "<leader>xm",
+      function()
+        require("nvim_make").pick_make_target()
+      end,
+      desc = "Run make target",
+    },
+  },
+}
