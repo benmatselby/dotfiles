18118eeec8ddb2f179d2bbe7f48fa3fe95b49c8a
Ben Selby <benmatselby@gmail.com>
2026-02-26 08:04:01 +0000

    Do not install skills to Claude
    Since I'm using opencode, and opencode looks there, I'll leave claude as
    a place where I put work skills

--- agents/install.sh
+++ agents/install.sh
@@ -4,8 +4,5 @@ set -e
 
 echo "🚀 agents configuration"
 
-mkdir -p "${HOME}/.claude"
-ln -shf "${PWD}/agents/skills" "${HOME}/.claude/skills"
-
 mkdir -p "${HOME}/.config/opencode"
 ln -shf "${PWD}/agents/skills" "${HOME}/.config/opencode/skills"
