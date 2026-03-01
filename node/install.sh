ee52006bf26f9fc131b8efdc84997c29b8976a91
Ben Selby <benmatselby@gmail.com>
2026-03-01 15:17:17 +0000

    Make node 24 the default

--- node/install.sh
+++ node/install.sh
@@ -7,17 +7,17 @@ echo "🚀 Node configuration"
 # Load nvm
 export NVM_DIR="$HOME/.nvm"
 if [ "$(arch)" = "arm64" ]; then
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 fi
 
 if [[ "$(uname -s)" == "Linux" ]]; then
-  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"                                       # This loads nvm
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 fi
 
 # The version of node mainly used
-ACTIVE_NODE_VERSION=22
+ACTIVE_NODE_VERSION=24
 
 echo "Installing ${ACTIVE_NODE_VERSION} version of node"
 nvm install ${ACTIVE_NODE_VERSION}
@@ -28,3 +28,6 @@ nvm alias default ${ACTIVE_NODE_VERSION}
 echo "Installing node packages"
 NODE_PACKAGES=()
 for pkg in "${NODE_PACKAGES[@]}"; do printf "installing %s\n" "${pkg}" && npm install -g "${pkg}"; done
+
+echo "Clear the cache of nvm - which gets massive"
+nvm cache clear
