e2906ca6d6d1f1eb5aed5343e31409a18ffc335d
Ben Selby <benmatselby@gmail.com>
2026-03-21 15:57:42 +0000

    Update the neovim config (plugins+keymaps)

--- neovim/lazy-vim/lua/config/keymaps.lua
+++ neovim/lazy-vim/lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("i", "<A-l>", "<C-o>w") -- Alt+L to jump word right
+vim.keymap.set("i", "<A-h>", "<C-o>b") -- Alt+H to jump word left
+
 -- Allow block moving up and down in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
