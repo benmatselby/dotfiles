f953a5037120f3b973e66ce15765c786f4bb9d4b
Ben Selby <benmatselby@gmail.com>
2026-03-22 09:45:05 +0000

    Document why things are deprecated

--- install.sh
+++ install.sh
@@ -10,23 +10,25 @@ set -e
 ./git/install.sh
 ./brew/install.sh
 ./github/install.sh
-./node/install.sh
 ./mongodb/install.sh
 ./k9s/install.sh
 ./tmux/install.sh
 ./ghostty/install.sh
 ./sqlite/install.sh
 ./agents/install.sh
+./neovim/install.sh
 ./sublime-text/install.sh
 
 ###
 # Deprecated
 ###
+# Moved to neovim
 # ./zed/install.sh
-# ./neovim/install.sh
-# ./php/install.sh
 # ./vim/install.sh
 # ./vscode/install.sh
+# Moved to mise
+# ./node/install.sh
+# ./php/install.sh
 
 ###
 # Manual notifications
