e3f2fed41bfe9bc75c3fc653ff05b96667c89ef5
Ben Selby <benmatselby@gmail.com>
2026-08-29 10:20:37 +0100

    Turn on transparency for nvim for a while
    See how we go

--- neovim/lazy-vim/lua/plugins/rose-pine.lua
+++ neovim/lazy-vim/lua/plugins/rose-pine.lua
@@ -5,6 +5,10 @@ return {
     require("rose-pine").setup({
       variant = "auto",
       dark_variant = "moon",
+
+      styles = {
+        transparency = true,
+      },
     })
     vim.cmd("colorscheme rose-pine")
   end,
