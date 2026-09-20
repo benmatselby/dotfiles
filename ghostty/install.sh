b524c625887906982a37fcfa685dd0e2917d2c35
Ben Selby <benmatselby@gmail.com>
2026-09-20 18:52:30 +0100

    Define the Ghostty config path for linux

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -6,6 +6,9 @@ echo "🚀 Ghostty configuration"
 
 PATH_TO_GHOSTTY="${HOME}/Library/Application Support/com.mitchellh.ghostty/"
 
+if [[ "$(uname -s)" == "Linux" ]]; then
+  PATH_TO_GHOSTTY="${HOME}/.config/ghostty"
+fi
 mkdir -p "${PATH_TO_GHOSTTY}"
 
 ln -sf "${PWD}/ghostty/config" "${PATH_TO_GHOSTTY}/config"
