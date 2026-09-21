2018629fb5b532c5f0e90665771f519074c7098e
Ben Selby <benmatselby@gmail.com>
2026-09-21 08:30:51 +0100

    Define the opencode config

--- agents/install.sh
+++ agents/install.sh
@@ -5,6 +5,7 @@ set -e
 echo "🚀 agents configuration"
 
 mkdir -p "${HOME}/.config/opencode"
+ln -snf "${PWD}/agents/opencode.json" "${HOME}/.config/opencode/opencode.json"
 ln -shf "${PWD}/agents/skills" "${HOME}/.config/opencode/skills"
 ln -shf "${PWD}/agents/plugins" "${HOME}/.config/opencode/plugins"
 ln -shf "${PWD}/agents/commands" "${HOME}/.config/opencode/commands"
